ab1afc0309fadce6aea8f99aabac66054e23ced0
Braden Walker <bradencodes@gmail.com>
2019-02-28 10:45:08 -0800

    add basic endpoints for Users

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
+const userRoutes = require('./users/userRoutes.js');
+
 const server = express();
 
 server.use(cors({ origin: 'http://localhost:3000' , credentials: true }));
@@ -13,12 +15,14 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: 'running...' })
 });
 
+server.use('/user', userRoutes);
+
 const port = process.env.PORT || 5000;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASSWORD}@ds040349.mlab.com:40349/connectfour`, { useNewUrlParser: true }, (error) => {
     if (error) console.log(error);
-    console.log("Mongoose connected to mLab connectfour DB");
+    else console.log("Mongoose connected to mLab connectfour DB");
 })
 
 server.listen(port, () => {
--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -4,9 +4,12 @@ const User = require('./userModel.js');
 
 //base route = '/user'
 
+//create a new user
+//input: none
+//output: the new user with `_id` and `games` fields
 router.route('/create')
     .post((req, res) => {
-        User.create()
+        User.create({})
             .then(newUser => {
                 res.status(201).json(newUser);
             })
@@ -14,3 +17,39 @@ router.route('/create')
                 res.status(500).json({ errorMessage: 'Could not create new user' });
             })
     })
+
+//find a user
+//input: _id
+//output: the user matching the _id
+router.route('/get')
+    .get((req, res) => {
+        const { _id } = req.body;
+
+        User.findById(_id)
+            .then(user => {
+                if (user) res.status(200).json(user)
+                else res.status(404).json({ errorMessage: 'User not found' });
+            })
+            .catch(err => {
+                res.status(500).json({ errorMessage: 'Database failed to look for user' });
+            })
+    })
+
+//delete a user
+//input: _id
+//output: the deleted user
+router.route('/delete')
+    .delete((req, res) => {
+        const { _id } = req.body;
+
+        User.findByIdAndDelete(_id)
+            .then(user => {
+                if (user) res.status(200).json(user);
+                else res.status(404).json({ errorMessage: 'User not found to delete' });
+            })
+            .catch(err => {
+                res.status(500).json({ errorMessage: 'Database failed to delete user' });
+            })
+    })
+
+module.exports = router;
