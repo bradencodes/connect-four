d9b08f1228702ccc57a8015e7a32162fb07e87a0
Braden Walker <bradencodes@gmail.com>
2019-02-27 16:07:31 -0800

    add router to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import reducers from './store/reducers';
+
+import Home from './views/Home.js';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
+      </Provider>
     );
   }
-}
+};
 
 export default App;
