ae1686abe2a4da6912f0720282676da06cd8460d
Braden Walker <bradencodes@gmail.com>
2019-03-01 09:54:05 -0800

    refactor server.js to work with socket.io

--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`\n=== API running on http://localhost:${port} ===\n`);
+})
 
 const userRoutes = require('./users/userRoutes.js');
 const gameRoutes = require('./games/gameRoutes.js');
 
-const server = express();
+app.use(cors({ origin: 'http://localhost:3000' , credentials: true }));
+app.use(express.json());
 
-server.use(cors({ origin: 'http://localhost:3000' , credentials: true }));
-server.use(express.json());
-
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).json({ api: 'running...' })
 });
 
-server.use('/user', userRoutes);
-server.use('/game', gameRoutes);
-
-const port = process.env.PORT || 5000;
+app.use('/user', userRoutes);
+app.use('/game', gameRoutes);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASSWORD}@ds040349.mlab.com:40349/connectfour`, { useNewUrlParser: true }, (error) => {
     if (error) console.log(error);
     else console.log("Mongoose connected to mLab connectfour DB");
-})
-
-server.listen(port, () => {
-    console.log(`\n=== API running on http://localhost:${port} ===\n`);
 })
