0931ba356568dbaba8b2ae7bf5f27d2105c9bc7f
Braden Walker <bradencodes@gmail.com>
2019-03-08 09:11:34 -0800

    board fully responsive

--- client/src/components/gameParts/Board.js
+++ client/src/components/gameParts/Board.js
@@ -6,22 +6,24 @@ import Col from './Col.js';
 
 const Board = (props) => {
     return (
-        <div className='board' >
-        
-            <img src={boardBack} className='board-back' alt='boardBack' />
-
-            <div className='columns'>
-                <Col num={1} allState={props.allState} updateAllState={props.updateAllState} />
-                <Col num={2} allState={props.allState} updateAllState={props.updateAllState}/>
-                <Col num={3} allState={props.allState} updateAllState={props.updateAllState}/>
-                <Col num={4} allState={props.allState} updateAllState={props.updateAllState}/>
-                <Col num={5} allState={props.allState} updateAllState={props.updateAllState}/>
-                <Col num={6} allState={props.allState} updateAllState={props.updateAllState}/>
-                <Col num={7} allState={props.allState} updateAllState={props.updateAllState}/>
-            </div>
+        <div className='board-container' >
+            <div className='board' >
             
-            <img src={boardFront} className='board-front' alt='boardFront' />
+                <img src={boardBack} className='board-back' alt='boardBack' />
 
+                <div className='columns'>
+                    <Col num={1} allState={props.allState} updateAllState={props.updateAllState} />
+                    <Col num={2} allState={props.allState} updateAllState={props.updateAllState}/>
+                    <Col num={3} allState={props.allState} updateAllState={props.updateAllState}/>
+                    <Col num={4} allState={props.allState} updateAllState={props.updateAllState}/>
+                    <Col num={5} allState={props.allState} updateAllState={props.updateAllState}/>
+                    <Col num={6} allState={props.allState} updateAllState={props.updateAllState}/>
+                    <Col num={7} allState={props.allState} updateAllState={props.updateAllState}/>
+                </div>
+                
+                <img src={boardFront} className='board-front' alt='boardFront' />
+
+            </div>
         </div>
     )
 }
