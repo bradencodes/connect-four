8d8ada7b9c2da2bbb38951858023feb4dae83be3
Braden Walker <bradencodes@gmail.com>
2019-03-12 11:52:27 -0700

    banner changes appropriately
    banner announces turn and winner

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,18 +5,37 @@ const Header = (props) => {
     let isGame = !!props.allState.game._id;
     let isWinner = isGame && props.allState.game.winner !== 'none';
     let isTurn = isGame ? props.allState.game.turn : null;
-    let bannerText = props.allState.game[isTurn] === props.allState.user._id 
-                        ? "YOUR TURN" : "OPPONENT'S TURN";
-    let turnBannerStyle = {
+    const turnBannerStyle = () => {
+        if (isWinner) {
+            let winner = props.allState.game.winner;
+            if (winner === 'tie') return {background: 'linear-gradient(90deg, rgba(212,0,0,1) 40%, rgba(51,51,51,1) 60%)'};
+            if (winner === 'red') return {background: '#d40000'};
+            return {background: '#333333'}
+        }
+        
+        return ({
         background: isTurn === 'red' ? '#d40000' : '#333333'
+        })
     }
+
+    const bannerText = () => {
+        if (isWinner) {
+            let winner = props.allState.game.winner;
+            if (winner === 'tie') return "TIE GAME";
+            if (props.allState.game[winner] === props.allState.user._id) return "YOU WIN!";
+            return "OPPONENT WINS"
+        }
+
+        return props.allState.game[isTurn] === props.allState.user._id 
+                ? "YOUR TURN" : "OPPONENT'S TURN";
+    } 
     return (
         <div className='header-container' >
             <div className='logo-container'>
                 <img className='logo' src={logo} alt='logo' />
             </div>
 
-            <div style={ turnBannerStyle } className='turn-banner'>{bannerText}</div>
+            <div style={ turnBannerStyle() } className='turn-banner'>{bannerText()}</div>
         </div>
     )
 }
