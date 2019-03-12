ce8ff271d56ee6ca2fcd5cc9d646728055a85783
Braden Walker <bradencodes@gmail.com>
2019-03-12 11:22:57 -0700

    confetti corresponds to winner color

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -8,6 +8,14 @@ import Footer from './Footer.js';
 
 class Game extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { 
+            width: 0, 
+            height: 0 
+        };
+    }
+
     componentDidMount() {
         if (this.props.allState.userIsValid && !this.props.allState.game._id) {
             this.props.history.push('/matching');
@@ -17,9 +25,31 @@ class Game extends Component {
             this.props.history.push('');
         }
 
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+
         let playerColor = this.props.allState.game.red === this.props.allState.user._id ? 'red' : 'black';
 
         this.props.updateAllState({ playerColor });
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions = () => {
+        this.setState({ width: window.innerWidth, height: window.innerHeight });
+    }
+
+    confettiColors = () => {
+        let winner = this.props.allState.game.winner;
+        let blackShades = ['#333333', '#666666', '#111111']
+        let redShades = ['#aa0000', '#ff5555', '#ff0000']
+        let bothShades = ['#333333', '#666666', '#111111', '#aa0000', '#ff5555', '#ff0000']
+
+        if (winner === 'red') return redShades;
+        if (winner === 'black') return blackShades;
+        return bothShades;
     }
 
     render() {
@@ -30,7 +60,8 @@ class Game extends Component {
             <div className="game-container">
                 {isWinner ? 
                     <div className='confetti'>
-                        <Confetti />
+                        <Confetti width={this.state.width} height={this.state.height}
+                        colors={ this.confettiColors() }/>
                     </div>
                     : null
                 }
