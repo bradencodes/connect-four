c09677efafdc8e954810215045212daee0f0a4e8
Braden Walker <bradencodes@gmail.com>
2019-03-12 21:01:01 -0700

    refactor and comment Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,64 +5,72 @@ import logo from '../assets/logo.svg';
 import redToken from '../assets/redToken.svg';
 import blackToken from '../assets/blackToken.svg';
 
+let user, userIsValid, updateAllState;
+
 class Home extends Component {
 
     componentDidMount() {
+        user = this.props.allState.user;
+        userIsValid = this.props.allState.userIsValid;
+        updateAllState = this.props.updateAllState;
+
         //if the client isn't a user, create a user for the client
-        if (!this.props.allState.user._id) {
+        if (!user._id) {
             axios.post(`${process.env.REACT_APP_API_URL}/user`)
                 .then(res => {
                     localStorage.setItem('USER_ID', res.data._id);
-                    this.props.updateAllState({ user: res.data, userIsValid: true, error: null });
+                    updateAllState({ user: res.data, userIsValid: true, error: null });
                 })
                 .catch(err => {
                     console.log(err);
-                    this.props.updateAllState({error: err});
+                    updateAllState({error: err});
                 })
         }
 
         //verify the user_id is valid
-        if (this.props.allState.user._id && !this.props.allState.userIsValid) {
-            axios.get(`${process.env.REACT_APP_API_URL}/user/${this.props.allState.user._id}`)
+        if (user._id && !userIsValid) {
+            axios.get(`${process.env.REACT_APP_API_URL}/user/${user._id}`)
                 .then(res => {
                     //if user_id is valid, set userIsValid to true
-                    if (res.data._id === this.props.allState.user._id) this.props.updateAllState({ user: res.data, userIsValid: true });
+                    if (res.data._id === user._id) updateAllState({ user: res.data, userIsValid: true });
 
-                    //else, create a valid user
+                    //else, create a new valid user
                     else {
                         axios.post(`${process.env.REACT_APP_API_URL}/user`)
                             .then(res => {
                                 localStorage.setItem('USER_ID', res.data._id);
-                                this.props.updateAllState({ user: res.data, userIsValid: true, error: null });
+                                updateAllState({ user: res.data, userIsValid: true, error: null });
                             })
                             .catch(err => {
                                 console.log(err);
-                                this.props.updateAllState({ error: err })
+                                updateAllState({ error: err })
                             })
                     }
                 })
                 .catch(err => {
+                    //if there's an error validating the user, create a new valid user
                     axios.post(`${process.env.REACT_APP_API_URL}/user`)
                         .then(res => {
                             localStorage.setItem('USER_ID', res.data._id);
-                            this.props.updateAllState({ user: res.data, userIsValid: true, error: null });
+                            updateAllState({ user: res.data, userIsValid: true, error: null });
                         })
                         .catch(err => {
                             console.log(err);
-                            this.props.updateAllState({ error: err })
+                            updateAllState({ error: err })
                         })
                 })
         }
     }
 
     componentDidUpdate() {
-        //if the user is in an open game, send the user to that endpoint
-        if (this.props.allState.user.games.length > 0) {
-            axios.get(`${process.env.REACT_APP_API_URL}/game/${this.props.allState.user.games[this.props.allState.user.games.length-1]}`)
+        //if the user's last game is still going, send the user to that endpoint
+        if (user.games.length > 0) {
+            //look at the last game in the user's game array
+            axios.get(`${process.env.REACT_APP_API_URL}/game/${user.games[user.games.length-1]}`)
                 .then (res => {
                     //if the winner hasn't been determined, have the user join that game
                     if (res.data.winner === "none") {
-                        this.props.updateAllState({ game: res.data });
+                        updateAllState({ game: res.data });
                         this.props.history.push(`/game/${res.data._id}`);
                     }
             })
